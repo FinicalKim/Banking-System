29923959cc495cb0a71f39c46088ec55086fd98e
Kim P. Finical <79591944+FinicalKim@users.noreply.github.com>
2021-10-27 02:48:56 -0700

    Add files via upload

---
+++ Program_2/tester.java
@@ -0,0 +1,22 @@
+package Program_2;
+
+public class tester {
+	
+	public static void main(String[] aregs){
+	int x = 75;
+	
+	System.out.println("Account type\t: Saving" + "\n" + 
+		"Account #\t: " + x + "\n" + 
+		"Balance \t: " + "$" + x + "\n" +
+		"Intereest rate \t: " + x + "%" + "\n" + 
+		"Credit limit \t: " + "$" + x);
+		
+		
+		
+		
+		
+		
+	}
+
+
+}
